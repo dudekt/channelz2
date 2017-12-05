3763713aa6118c8f5bdd9894852bc924147ba515
Tadeusz Dudek <tadeusz.dudek@vml.com>
2017-12-05 16:05:36 +0100

    Update.

--- src/components/mainScreen/index.jsx
+++ src/components/mainScreen/index.jsx
@@ -18,18 +18,40 @@ const word = randomWord()
 const wordLetters = word.split('')
 
 export default class MainScreen extends React.Component {
-    render() {
 
+    constructor() {
+        super();
+        this.state = {
+            typedLetter: ''
+        }
+    }    
+
+    componentDidMount() {
+        document.addEventListener('keydown', (e) => {
+            if(keyCodes[e.keyCode] === wordLetters[this.state.typedLetter.length]){
+                keyCodes[e.keyCode]
+                this.setState({
+                    typedLetter: this.state.typedLetter + keyCodes[e.keyCode]
+                }) 
+            } else {
+                this.setState({
+                    typedLetter: ''
+                })
+            }
+        })
+    }
+
+    render() {
         return <div className={style['body']}>
             {wordLetters && 
-                wordLetters.map((letter) => {
+                wordLetters.map((letter, index) => {
                     return [
                         <span
                             key={letter}
-                            ref={(node) =>{
-                                Mousetrap.bind(letter, () => {node.classList.add(style.test)})
-                                console.log('dupa', letter)
-                            }}
+                            className={this.state.typedLetter.includes(wordLetters.slice(0, index + 1).join(''))
+                                ? style.test
+                                : ''
+                            }
                         >
                             {letter}
                         </span>
