675a4555ef68b8d7232a5316830d6ca56c7aeaa5
Tadeusz Dudek <dudek.t@gmail.com>
2017-12-05 19:12:38 +0100

    Update font size.

--- src/components/mainScreen/index.jsx
+++ src/components/mainScreen/index.jsx
@@ -4,17 +4,21 @@ import Mousetrap from 'mousetrap'
 import { keyCodes } from 'Components/keyCodes/'
 import style from './style.scss'
 
-const words = [
+let words = [
     'skurwol',
     'skurwolik',
     'skurwolix'
 ]
 
 const randomWord = () => {
-    return words[Math.floor(Math.random() * words.length)]
+    let random = words[Math.floor(Math.random() * words.length)]
+    let index = words.indexOf(random)
+    console.log(words.splice(index, 1), words)
+
+    return random
 }
 
-const word = randomWord()
+let word = randomWord()
 const wordLetters = word.split('')
 
 export default class MainScreen extends React.Component {
@@ -28,11 +32,18 @@ export default class MainScreen extends React.Component {
 
     componentDidMount() {
         document.addEventListener('keydown', (e) => {
-            if(keyCodes[e.keyCode] === wordLetters[this.state.typedLetter.length]){
+            let letterLength = wordLetters.length
+            console.log(letterLength, this.state.typedLetter.length)
+            if (keyCodes[e.keyCode] === wordLetters[this.state.typedLetter.length]) {
                 keyCodes[e.keyCode]
                 this.setState({
                     typedLetter: this.state.typedLetter + keyCodes[e.keyCode]
-                }) 
+                })
+
+                if (letterLength === this.state.typedLetter.length) {
+                    console.log('nowe słowo')
+                }
+
             } else {
                 this.setState({
                     typedLetter: ''
@@ -43,21 +54,26 @@ export default class MainScreen extends React.Component {
 
     render() {
         return <div className={style['body']}>
-            {wordLetters && 
-                wordLetters.map((letter, index) => {
-                    return [
-                        <span
-                            key={letter}
-                            className={this.state.typedLetter.includes(wordLetters.slice(0, index + 1).join(''))
-                                ? style.test
-                                : ''
-                            }
-                        >
-                            {letter}
-                        </span>
-                    ]
-                })
-            }
+            {wordLetters.map((letter, index) => {
+
+                let letterLength = wordLetters.length
+                console.log(letterLength)
+
+                return [
+                    <span
+                        key={letter}
+                        className={this.state.typedLetter.includes(wordLetters.slice(0, index + 1).join(''))
+                            ? style.test
+                            : ''
+                        }
+                        style={{
+                            fontSize: `${200 / letterLength}vh`
+                        }}
+                    >
+                        {letter}
+                    </span>
+                ]
+            })}
         </div>
     }
 }
