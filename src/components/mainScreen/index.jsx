3a2322325c757c73073ad7d7efad62b0de7cc29a
Tadeusz Dudek <dudek.t@gmail.com>
2017-12-11 19:04:52 +0100

    Update.

--- src/components/mainScreen/index.jsx
+++ src/components/mainScreen/index.jsx
@@ -4,17 +4,16 @@ import Mousetrap from 'mousetrap'
 import { keyCodes } from 'Components/keyCodes/'
 import style from './style.scss'
 
-let words = [
+let wordsCollection = [
     'skurwol',
     'skurwolik',
     'skurwolix'
 ]
 
 const randomWord = () => {
-    let random = words[Math.floor(Math.random() * words.length)]
-    let index = words.indexOf(random)
-    console.log(words.splice(index, 1), words)
-    words.splice(index, 1)
+    let random = wordsCollection[Math.floor(Math.random() * wordsCollection.length)]
+    let index = wordsCollection.indexOf(random)
+    wordsCollection.splice(index, 1)
 
     return random
 }
@@ -38,7 +37,6 @@ export default class MainScreen extends React.Component {
         document.addEventListener('keydown', (e) => {
             let letterLength = wordLetters.length
 
-            console.log('skurwol', )
             if (keyCodes[e.keyCode] === wordLetters[this.state.typedLetter.length]) {
                 keyCodes[e.keyCode]
                 this.setState({
@@ -47,7 +45,6 @@ export default class MainScreen extends React.Component {
 
                 if (letterLength === this.state.typedLetter.length) {
                     console.log('nowe słowo')
-                    document.getElementById('chuj').innerHTML = ''
                     let word = randomWord()
                 }
 
@@ -70,7 +67,7 @@ export default class MainScreen extends React.Component {
     }
 
     render() {
-        return <div id='chuj' className={style.body}>
+        return <div className={style.body}>
             {wordLetters.map((letter, index) => {
 
                 let letterLength = wordLetters.length
